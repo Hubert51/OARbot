0840c29999a068adf4fdb7450f533ffd879e1c4c
Hubert51 <gengr@rpi.edu>
2019-07-30 22:37:04 -0600

    update the train process

--- vision/RCNN.m
+++ vision/RCNN.m
@@ -34,11 +34,16 @@
 % 
 % rcnn = trainRCNNObjectDetector(trainingFood, layers, options, 'NegativeOverlapRange', [0 0.3]);
 
+load('rcnn_model_water.mat')
+rcnn_water = rcnn;
 load('rcnn_model.mat')
+rcnn_food = rcnn;
+load('rcnn_food_water.mat')
+rcnn_food_water;
 camera = RobotRaconteur.Connect('tcp://localhost:2345/KinovaCameraServer/Camera');
 robotArm = RobotRaconteur.Connect('tcp://localhost:4567/KinovaJointServer/Kinova');
 
-% img = imread('train_data/output00001.png');
+% img = imread('train_data/output00001.png');/
 % % img = imread('../test_data/test6.png');
 % 
 % [bbox, score, label] = detect(rcnn, img, 'MiniBatchSize', 32);
@@ -65,9 +70,9 @@ robotArm = RobotRaconteur.Connect('tcp://localhost:4567/KinovaJointServer/Kinova
 
 % define constant
 % object dimension
-obj_width = 8;
-obj_height = 8;
-offset = [4, 0];
+obj_width = 7;
+obj_height = 10;
+offset = [-0, 0];
 hardcode_ori = [-0.5141; -0.5357; -0.4807; -0.4666];
 
 while 1
@@ -80,38 +85,55 @@ while 1
 %     s3 = '.png';
 %     s = strcat(s1,s2, s3);
 %     imwrite(img, s)
+%     [bboxes, scores, label] = detect(rcnn_food_water, img, 'MiniBatchSize', 32);
+%     for i = 1:size(scores)
+%         annotation = sprintf('%s: (Confidence = %f)', label(i), scores(i));
+%         img = insertObjectAnnotation(img, 'rectangle', bboxes(i,:), annotation);
+%     end 
+%     imshow(img)
 
-    [bbox, score, label] = detect(rcnn, img, 'MiniBatchSize', 32);
-    [score, idx] = max(score);
+    detectedImg = objDetection(rcnn_water, img, ['food', 'water']);
+    imshow(detectedImg)
 
-    bbox = bbox(idx, :);
-    disp(bbox)
-    pix_len = (obj_width/bbox(3) + obj_height/bbox(4)) / 2;
-    desired = [640, 480] / 2 - [bbox(3), bbox(4)]/2;
-    % adjust same direction with error.
-    error = ( (desired-[bbox(1), bbox(2)])*pix_len + offset ) / 100;
-    pos = robotArm.getPos();
-    pos(2) = pos(2) + error(1);
-    robotArm.cartesian_pose_client(pos, hardcode_ori);
+%     detectedImg = objDetection(rcnn_food, detectedImg);
+%     imshow(detectedImg)
+%     prompt = 'Do you agree this detection ';
+    str = input(prompt,'s');
+    if isempty(str)
+        a = camera.getDepth(bbox') / 1000;
+        break
+    end
+    % drawnow
+    pause(0.1)
     
-    a = camera.getDepth(bbox') / 1000;
-    pos = robotArm.getPos() + [a-0.022; 0; 0.1];
-    robotArm.cartesian_pose_client(pos, hardcode_ori);
-
-
-    annotation = sprintf('%s: (Confidence = %f)', label(idx), score);
-
-    detectedImg = insertObjectAnnotation(img, 'rectangle', bbox, annotation);
 %     s1 = 'demo_data/';
 %     s = strcat(s1,s2, s3);
 %     imwrite(detectedImg, s)
 %     count = count + 1;
 %     save('count.mat', 'count');
-
-
-    
-    imshow(detectedImg)
-    drawnow
-
-    pause(0.1)
 end
+
+% move the arm
+% move arm in y z direction
+pix_len = (obj_width/bbox(3) + obj_height/bbox(4)) / 2;
+desired = [640, 480] / 2 - [bbox(3), bbox(4)]/2;
+% adjust same direction with error.
+error = ( (desired-[bbox(1), bbox(2)])*pix_len + offset ) / 100;
+a = camera.getDepth(bbox') / 1000;
+
+pos = robotArm.getPos();
+pos(2) = pos(2) + error(1);
+depth = sqrt(a^2 - error(1)^2)
+% robotArm.cartesian_pose_client(pos, hardcode_ori);
+pause(1.5)
+% move arm in x direction
+pos = robotArm.getPos() + [depth-0.005; 0; 0.0];
+robotArm.cartesian_pose_client(pos, hardcode_ori);
+
+pause(1.5)
+robotArm.closeFinger([3600.0; 3600.0; 3600.0])
+pause(1.5)
+
+pos = robotArm.getPos() + [0; 0; 0.2];
+robotArm.cartesian_pose_client(pos, hardcode_ori);
+
