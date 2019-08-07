108313c3485f8a5a08ea76c9595422c9072c0bf0
Hubert51 <gengr@rpi.edu>
2019-08-07 18:05:58 -0400

    small changes in object detection and localization

--- vision/objDetection.m
+++ vision/objDetection.m
@@ -1,55 +1,63 @@
-function [bbox] = objDetection(model, iteration)
+function [result] = objDetection(model, objects, iteration)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
     count = 0;
     final_score = 0;
     bbox = [0 0 0 0];
+    result = [];
+    for j=1:length(objects) 
+        object.name = objects(j);
+        object.bbox = [0 0 0 0];
+        object.count = 0;
+        result = [result, object];
+    end
     for i = 1:iteration
         imgSub = rossubscriber('/camera/color/image_raw');
         imgMsg = receive(imgSub);
         img = readImage(imgMsg);
         [bboxes, scores, label] = detect(model, img, 'MiniBatchSize', 32);
-        %% food:
-    %     index = label=='food';
-    %     mask=bboxes > [0 0 50 50];
-    %     mask = prod(mask, 2);
-    %     [score, idx] = max(mask .* scores .* index);
-    %     bbox = bboxes(idx, : );
-    %     annotation = sprintf('%s: (Confidence = %f)', label(idx), score);
-    %     img = insertObjectAnnotation(img, 'rectangle', bbox, annotation);
 
         %% water
-        % index = label=='water';
-        mask=bboxes > [0 0 80 80];
-        mask = prod(mask, 2);
-        [score, idx] = max(mask .* scores);
+        for j=1:length(objects) 
+            index = label==objects(j);
+            mask=bboxes > [0 0 80 80];
+            mask = prod(mask, 2);
+            [score, idx] = max(mask .* scores .* index);
 
 
-        if score >0.5
-            annotation = sprintf('%s: (Confidence = %f)', label(idx), score);
-            final_score = final_score + score;
-            img = insertObjectAnnotation(img, 'rectangle', bboxes(idx,:), annotation);
-            bboxes(idx,:)
-            drawnow
-            imshow(img)
-            count = count + 1;
-            bbox = bbox + bboxes(idx, : );
-%           annotation = sprintf('%s: (Confidence = %f)', label(idx), score);
-%         img = insertObjectAnnotation(img, 'rectangle', bbox, annotation);
-% 
-         % imshow(img)
+            if score >0.5
+%                 annotation = sprintf('%s: (Confidence = %f)', label(idx), score);
+%                 final_score = final_score + score;
+%                 img = insertObjectAnnotation(img, 'rectangle', bboxes(idx,:), annotation);
+%                 bboxes(idx,:)
+%                 drawnow
+%                 imshow(img)
+                result(j).count = result(j).count + 1;
+                result(j).bbox = result(j).bbox + bboxes(idx, : );
+    %           annotation = sprintf('%s: (Confidence = %f)', label(idx), score);
+    %         img = insertObjectAnnotation(img, 'rectangle', bbox, annotation);
+    % 
+             % imshow(img)
+            end
         end
     end
-    if count >= iteration/3
-        bbox = bbox / count;
-    else
-        bbox = [0 0 0 0];
-    end
-    annotation = sprintf('%s: (Confidence = %f)', label(idx), final_score/count);
-    img = insertObjectAnnotation(img, 'rectangle', bbox, annotation);
     
+    for j = 1:length(result)
+        if result(j).count >= iteration/3
+            result(j).bbox = result(j).bbox / result(j).count;
+        else
+            result(j).bbox = [0 0 0 0];
+        end
+    end
+    
+    for j = 1:length(result)
+        annotation = sprintf('%s: (Confidence = %f)', result(j).name, 1.00);
+        img = insertObjectAnnotation(img, 'rectangle', result(j).bbox, annotation);
+    end
+
     imshow(img)
     drawnow
+
 end
 
 %     while ~(bbox(3) > 50 && bbox(4) > 50)
--- vision/objLocalization.m
+++ vision/objLocalization.m
@@ -1,4 +1,4 @@
-function [result_shift] = objLocalization(rcnn, bbox1)
+function [result] = objLocalization(rcnn, bbox1)
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
     camera_shift = 0.02;
@@ -10,25 +10,55 @@ function [result_shift] = objLocalization(rcnn, bbox1)
     % bbox1 = objDetection(rcnn, 8);
     offset = [0.05; 0; 0];
 
-    if bbox1(1)+bbox1(3)/2 < 320
-        dir = 1;
+    robotArm.closeFinger([0.0; 0.0; 0.0]);
+    imgSub = rossubscriber('/camera/color/image_raw');
+    imgMsg = receive(imgSub);
+    img = readImage(imgMsg);
+    bbox = objDetection(rcnn, 8);
+    if sum(bbox) == 0
+        result = NULL;
+
     else
-        dir = -1;
+        debug_depth = -camera.getDepth(bbox')/1000; 
+
+        if bbox1(1)+bbox1(3)/2 < 320
+            dir = 1;
+        else
+            dir = -1;
+        end
+
+        robotArm.cartesian_pose_client(offset*dir, cur_ori, 1);
+        bbox2 = objDetection(rcnn, 8);
+        shift = bbox2 - bbox1;
+
+        % mvoe back
+        robotArm.cartesian_pose_client(-offset*dir, cur_ori, 1);
+        pixel_len = abs(0.05 / shift(1))
+        img_center = [640 / 2; 0; 480/2] ;
+        obj_center = [bbox1(1) + bbox1(3)/2; 0; bbox1(2) + bbox1(4)/2];
+
+        result_shift = (obj_center-img_center)*pixel_len .* [-1*dir*dir; 0; 1];
+        trans = result_shift + [camera_shift; 0; height_shift];
+        % result_shift = result_shift + [camera_shift; -camera.getDepth(bbox1')/1000+0.008; height_shift];
+        
+        robotArm.cartesian_pose_client(trans, init_ori, 1);
+        robotArm.cartesian_pose_client([0; debug_depth; 0], grab_ori, 1);
+
+        pause(0.5)
+        robotArm.closeFinger([6000.0; 6000.0; 6000.0])
+        pause(1)
+        robotArm.cartesian_pose_client(cur_pos, cur_ori, 0);
+%         robotArm.cartesian_pose_client(second_pos, init_ori, 0);
+%         robotArm.cartesian_pose_client(second_grab+[0;0;0.05], init_ori, 0);
+%         robotArm.closeFinger([0.0; 0.0; 0.0])
+%         pause(1)
+%         robotArm.closeFinger([0.0; 0.0; 0.0])
+%         pause(1)
+%         robotArm.cartesian_pose_client(second_pos, init_ori, 0);
+%         robotArm.cartesian_pose_client(init_pos, init_ori, 0);
     end
 
-    robotArm.cartesian_pose_client(offset*dir, cur_ori, 1);
-    bbox2 = objDetection(rcnn, 8);
-    shift = bbox2 - bbox1;
-    
-    % mvoe back
-    robotArm.cartesian_pose_client(-offset*dir, cur_ori, 1);
-    pixel_len = abs(0.05 / shift(1))
-    img_center = [640 / 2; 0; 480/2] ;
-    obj_center = [bbox1(1) + bbox1(3)/2; 0; bbox1(2) + bbox1(4)/2];
-    
-    result_shift = (obj_center-img_center)*pixel_len .* [-1*dir*dir; 0; 1];
-    result_shift = result_shift + [camera_shift; 0; height_shift];
-    % result_shift = result_shift + [camera_shift; -camera.getDepth(bbox1')/1000+0.008; height_shift];
+
 
 
 end
